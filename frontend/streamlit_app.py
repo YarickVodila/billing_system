7df99ce0eee04a4b67640cfa4b12ec1b9b238ef8
YarickVodila <y.koltashev@mail.ru>
2025-04-26 13:34:47 +0500

    edit streamlit app

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -1,114 +1,15 @@
+from streamlit_helper import register_user, login_user, get_current_user, replenish_balance, make_prediction, get_statistics, get_transaction_history
 import os
 import streamlit as st
-import requests
 from datetime import datetime
 import pandas as pd
 
-
-# FastAPI backend URL
-BASE_URL = os.getenv("BACKEND_FASTAPI", "http://localhost:8000") # "http://localhost:8000"  # Change this if your backend is hosted elsewhere
 
 # Initialize session state
 if 'access_token' not in st.session_state:
     st.session_state.access_token = None
 if 'current_user' not in st.session_state:
     st.session_state.current_user = None
-
-# Helper functions for API calls
-def register_user(username, email, password):
-    url = f"{BASE_URL}/register"
-    data = {
-        "username": username,
-        "email": email,
-        "password": password,
-        # "balance": balance
-    }
-    try:
-        response = requests.post(url, json=data)
-        if response.status_code == 200:
-            return True, response.json()["message"]
-        else:
-            return False, response.json().get("detail", "Registration failed")
-    except Exception as e:
-        return False, f"Error: {str(e)}"
-
-def login_user(username, password):
-    url = f"{BASE_URL}/login"
-    try:
-        # response = requests.post(url, data={"username": username, "password": password})
-        response = requests.post(
-            url, 
-            json={"username": username, "password": password}
-        )
-
-        if response.status_code == 200:
-            st.session_state.access_token = response.json()["access_token"]
-            return True, "Login successful"
-        else:
-            return False, response.json().get("detail", "Login failed")
-    except Exception as e:
-        return False, f"Error: {str(e)}"
-
-def get_current_user():
-    url = f"{BASE_URL}/user"
-    headers = {"Authorization": f"Bearer {st.session_state.access_token}"}
-    try:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            st.session_state.current_user = response.json()
-            return True, response.json()
-        else:
-            return False, response.json().get("detail", "Failed to get user info")
-    except Exception as e:
-        return False, f"Error: {str(e)}"
-
-def replenish_balance(amount):
-    url = f"{BASE_URL}/balance_replenish"
-    headers = {"Authorization": f"Bearer {st.session_state.access_token}"}
-    try:
-        response = requests.post(url, headers=headers, params={"amount": amount})
-        if response.status_code == 200:
-            return True, response.json()["message"]
-        else:
-            return False, response.json().get("detail", "Balance replenish failed")
-    except Exception as e:
-        return False, f"Error: {str(e)}"
-
-def make_prediction(model_id, data):
-    url = f"{BASE_URL}/predict"
-    headers = {"Authorization": f"Bearer {st.session_state.access_token}"}
-    try:
-        response = requests.post(url, headers=headers, params={"model_id": model_id}, json=data)
-        if response.status_code == 200:
-            return True, response.json()
-        else:
-            return False, response.json().get("detail", "Prediction failed")
-    except Exception as e:
-        return False, f"Error: {str(e)}"
-
-def get_statistics():
-    url = f"{BASE_URL}/get_statistic"
-    headers = {"Authorization": f"Bearer {st.session_state.access_token}"}
-    try:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            return True, response.json()["data"]
-        else:
-            return False, response.json().get("detail", "Failed to get statistics")
-    except Exception as e:
-        return False, f"Error: {str(e)}"
-
-def get_transaction_history(trans_type):
-    url = f"{BASE_URL}/get_history_transaction"
-    headers = {"Authorization": f"Bearer {st.session_state.access_token}"}
-    try:
-        response = requests.get(url, headers=headers, params={"type_trans": trans_type})
-        if response.status_code == 200:
-            return True, response.json()["data"]
-        else:
-            return False, response.json().get("detail", "Failed to get transaction history")
-    except Exception as e:
-        return False, f"Error: {str(e)}"
 
 # Streamlit UI
 st.set_page_config(page_title="Credit Prediction App", layout="wide")
@@ -117,19 +18,19 @@ st.set_page_config(page_title="Credit Prediction App", layout="wide")
 def main():
     st.title("Credit Prediction Application")
     st.markdown("""
-    This application interacts with a FastAPI backend to:
-    - Register and authenticate users
-    - Make credit predictions using different ML models
-    - View prediction history and transactions
+    Это приложение взаимодействует с серверной частью FastAPI для:
+    - регистрации и аутентификации пользователей
+    - составления кредитных прогнозов с использованием различных моделей машинного обучения
+    - просмотра истории прогнозов и транзакций
     """)
 
     # Navigation
-    menu = ["Home", "Login", "Register", "Dashboard", "Make Prediction", "History"]
+    menu = ["Home", "Login", "Register", "Личный кабинет", "Make Prediction", "History"]
     if st.session_state.access_token:
         menu.remove("Login")
         menu.remove("Register")
     else:
-        menu.remove("Dashboard")
+        menu.remove("Личный кабинет")
         menu.remove("Make Prediction")
         menu.remove("History")
 
@@ -159,13 +60,13 @@ def main():
                 st.warning("Please enter both username and password")
 
     elif choice == "Register":
-        st.subheader("Register New User")
+        st.subheader("Регистрация нового пользователя")
         username = st.text_input("Username")
         email = st.text_input("Email")
         password = st.text_input("Password", type="password")
         # balance = st.number_input("Initial Balance", min_value=0, value=1000)
         
-        if st.button("Register"):
+        if st.button("Зарегистрироваться"):
             if username and email and password:
                 success, message = register_user(username, email, password)
                 if success:
@@ -173,19 +74,19 @@ def main():
                 else:
                     st.error(message)
             else:
-                st.warning("Please fill all fields")
+                st.warning("Пожалуйста заполните все поля")
 
-    elif choice == "Dashboard":
-        st.subheader("User Dashboard")
+    elif choice == "Личный кабинет":
+        st.subheader("Личный кабинет")
         if st.session_state.current_user:
             user = st.session_state.current_user
-            st.write(f"### Welcome, {user['username']}!")
-            st.write(f"#### Your current balance: ${user['balance']}")
+            st.write(f"### Добро пожаловать, {user['username']}!")
+            st.write(f"#### Ваш баланс в мёртвых президентах: ${user['balance']}")
             
             # Balance replenishment
-            st.write("### Replenish Balance")
-            amount = st.number_input("Amount to add", min_value=1, value=100)
-            if st.button("Add Funds"):
+            st.write("### Пополнить баланс на халяву")
+            amount = st.number_input("Количество мёртвых президентов", min_value=1, value=100)
+            if st.button("Пополнить"):
                 success, message = replenish_balance(amount)
                 if success:
                     st.success(message)
@@ -201,10 +102,10 @@ def main():
         st.subheader("Make a Prediction")
         if st.session_state.current_user:
             user = st.session_state.current_user
-            st.write(f"Your current balance: ${user['balance']}")
+            st.write(f"Ваш баланс в мёртвых президентах: ${user['balance']}")
             
             # Model selection
-            model_id = st.radio("Select Model", 
+            model_id = st.radio("Выберите модель", 
                                 options=["1", "2", "3"],
                                 format_func=lambda x: {
                                     "1": "Linear Regression (Cost: $10)",
@@ -213,27 +114,45 @@ def main():
                                 }[x])
             
             # Input form
-            st.write("### Enter Prediction Data")
+            st.write("### Заполните форму")
             with st.form("prediction_form"):
                 col1, col2 = st.columns(2)
                 
+                loan_intent_hash = {
+                    "Предпринимательство": 'VENTURE', 
+                    "Медицина": 'MEDICAL', 
+                    "Образование": 'EDUCATION', 
+                    "Закрытие долгов": 'DEBTCONSOLIDATION', 
+                    "Личное": 'PERSONAL', 
+                    "Ремонт дома": 'HOMEIMPROVEMENT'
+                }
+
+                cb_person_default_on_file_hash = {
+                    "Да": "Y",
+                    "Нет": "N"
+                }
+
                 with col1:
-                    person_age = st.number_input("Age", min_value=18, max_value=100, value=30)
-                    person_income = st.number_input("Annual Income ($)", min_value=0, value=50000)
-                    person_emp_length = st.number_input("Employment Length (years)", min_value=0, value=5)
-                    loan_intent = st.selectbox("Loan Intent", ["EDUCATION", "MEDICAL", "VENTURE", "PERSONAL", "DEBTCONSOLIDATION", "HOMEIMPROVEMENT"])
-                    loan_grade = st.selectbox("Loan Grade", ["A", "B", "C", "D", "E", "F", "G"])
+                    person_age = st.number_input("Количество лет", min_value=0, value=30)
+                    person_income = st.number_input("Заработная плата в год ($)", min_value=0, value=50000)
+                    person_emp_length = st.number_input("Опыт работы (кол-во лет)", min_value=0, value=5)
+                    loan_intent = st.selectbox("На что нужен кредит", ['Предпринимательство', 'Медицина', 'Образование', 'Закрытие долгов', 'Личное', 'Ремонт дома'])
+                    loan_grade = st.selectbox("Кредитный рейтинг", ['A', 'C', 'B', 'D', 'E', 'F', 'G'])
                 
                 with col2:
-                    loan_amnt = st.number_input("Loan Amount ($)", min_value=0, value=10000)
-                    loan_int_rate = st.number_input("Interest Rate (%)", min_value=0.0, max_value=100.0, value=5.0)
-                    loan_percent_income = st.number_input("Loan Percent of Income", min_value=0.0, max_value=1.0, value=0.2)
-                    cb_person_default_on_file = st.selectbox("Previous Default", ["Y", "N"])
-                    cb_person_cred_hist_length = st.number_input("Credit History Length (years)", min_value=0, value=3)
+                    loan_amnt = st.number_input("Сумма кредита ($)", min_value=0, value=10000)
+                    loan_int_rate = st.number_input("Процентная ставка (%)", min_value=0.0, value=5.0)
+                    loan_percent_income = st.number_input("Процент от дохода на погашение кредита", min_value=0.0,  value=0.2)
+                    cb_person_default_on_file = st.selectbox("Были ли задолженности", ["Да", "Нет"])
+                    cb_person_cred_hist_length = st.number_input("Кредитная история (кол-во лет)", min_value=0, value=3)
                 
                 submitted = st.form_submit_button("Make Prediction")
                 
                 if submitted:
+                    
+                    loan_intent = loan_intent_hash[loan_intent]
+                    cb_person_default_on_file = cb_person_default_on_file_hash[cb_person_default_on_file]
+
                     data = {
                         "person_age": person_age,
                         "person_income": person_income,
---
+++ frontend/streamlit_helper.py
@@ -0,0 +1,102 @@
+import os
+import requests
+import streamlit as st
+
+# FastAPI backend URL
+BASE_URL = os.getenv("BACKEND_FASTAPI", "http://localhost:8000")
+
+# Helper functions for API calls
+def register_user(username, email, password):
+    url = f"{BASE_URL}/register"
+    data = {
+        "username": username,
+        "email": email,
+        "password": password,
+        # "balance": balance
+    }
+    try:
+        response = requests.post(url, json=data)
+        if response.status_code == 200:
+            return True, response.json()["message"]
+        else:
+            return False, response.json().get("detail", "Registration failed")
+    except Exception as e:
+        return False, f"Error: {str(e)}"
+
+def login_user(username, password):
+    url = f"{BASE_URL}/login"
+    try:
+        # response = requests.post(url, data={"username": username, "password": password})
+        response = requests.post(
+            url, 
+            json={"username": username, "password": password}
+        )
+
+        if response.status_code == 200:
+            st.session_state.access_token = response.json()["access_token"]
+            return True, "Login successful"
+        else:
+            return False, response.json().get("detail", "Login failed")
+    except Exception as e:
+        return False, f"Error: {str(e)}"
+
+def get_current_user():
+    url = f"{BASE_URL}/user"
+    headers = {"Authorization": f"Bearer {st.session_state.access_token}"}
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            st.session_state.current_user = response.json()
+            return True, response.json()
+        else:
+            return False, response.json().get("detail", "Failed to get user info")
+    except Exception as e:
+        return False, f"Error: {str(e)}"
+
+def replenish_balance(amount):
+    url = f"{BASE_URL}/balance_replenish"
+    headers = {"Authorization": f"Bearer {st.session_state.access_token}"}
+    try:
+        response = requests.post(url, headers=headers, params={"amount": amount})
+        if response.status_code == 200:
+            return True, response.json()["message"]
+        else:
+            return False, response.json().get("detail", "Balance replenish failed")
+    except Exception as e:
+        return False, f"Error: {str(e)}"
+
+def make_prediction(model_id, data):
+    url = f"{BASE_URL}/predict"
+    headers = {"Authorization": f"Bearer {st.session_state.access_token}"}
+    try:
+        response = requests.post(url, headers=headers, params={"model_id": model_id}, json=data)
+        if response.status_code == 200:
+            return True, response.json()
+        else:
+            return False, response.json().get("detail", "Prediction failed")
+    except Exception as e:
+        return False, f"Error: {str(e)}"
+
+def get_statistics():
+    url = f"{BASE_URL}/get_statistic"
+    headers = {"Authorization": f"Bearer {st.session_state.access_token}"}
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return True, response.json()["data"]
+        else:
+            return False, response.json().get("detail", "Failed to get statistics")
+    except Exception as e:
+        return False, f"Error: {str(e)}"
+
+def get_transaction_history(trans_type):
+    url = f"{BASE_URL}/get_history_transaction"
+    headers = {"Authorization": f"Bearer {st.session_state.access_token}"}
+    try:
+        response = requests.get(url, headers=headers, params={"type_trans": trans_type})
+        if response.status_code == 200:
+            return True, response.json()["data"]
+        else:
+            return False, response.json().get("detail", "Failed to get transaction history")
+    except Exception as e:
+        return False, f"Error: {str(e)}"
